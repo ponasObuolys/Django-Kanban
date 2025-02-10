e70924f0182dd624f1fb14e2458d86969e372a7e
Aurimas Butvilauskas <67857119+ponasObuolys@users.noreply.github.com>
2025-02-10 13:18:57 +0200

    mini pataisymas

--- boards/views/task_views.py
+++ boards/views/task_views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
+from django.contrib.auth import get_user_model
 from ..models import Task, Column, Board
 from ..forms import TaskForm, TaskCommentForm, TaskAttachmentForm
 from ..services.task_service import TaskService
